b6eaad24ac6897c1473bf9013bf2afd401811bc3
Max Kreminski <maxkreminski@gmail.com>
2014-03-12 07:28:40 -0700

    Simplify: let keyword-invoke syntax be desugared at runtime
    Instead of manually converting forms resembling (:kw target) to the
    canonical (get target :kw) form, rely on the fact that keywords implement
    IFn and can therefore be "invoked" at runtime without requiring any
    special support by the compiler.

--- src/cljs/clueless/expander.cljs
+++ src/cljs/clueless/expander.cljs
@@ -8,15 +8,6 @@
 
 ;; desugaring expanders
 
-(defn expand-keyword-invoke
-  "Desugars keyword invoke syntax `(:kw target)` to the canonical equivalent
-  `(get target :kw)` and returns the result."
-  [form]
-  (if (and (coll? form) (= (count form) 2))
-      (let [[kw target] form]
-        (list 'get target kw))
-      (raise "invalid keyword invoke syntax" form)))
-
 (defn expand-field-access [field-name target]
   (list '. target field-name))
 
@@ -58,10 +49,8 @@
   its first element, applies the corresponding macro to the remaining elements
   of `form` and returns the result; otherwise, returns `form`."
   [form]
-  (if (list? form)
-      (cond (symbol? (first form)) (-> form expand-macro expand-js-interop)
-            (keyword? (first form)) (expand-keyword-invoke form)
-            :else form)
+  (if (and (list? form) (symbol? (first form)))
+      (-> form expand-macro expand-js-interop)
       form))
 
 (defn expand
