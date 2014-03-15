9f1227f53fa17e5ae322847f142de92ad0bb3165
Max Kreminski <maxkreminski@gmail.com>
2014-03-15 11:32:25 -0700

    Use multimethods to clean up emitter code

--- src/cljs/ceci/emitter.cljs
+++ src/cljs/ceci/emitter.cljs
@@ -3,6 +3,8 @@
 
 (declare emit)
 
+;; helper fns
+
 (defn emit-escaped [s]
   (-> (name s)
     (string/replace #"\+" "_PLUS_")
@@ -18,40 +20,48 @@
 (defn emit-statements [statements]
   (string/join (map emit statements)))
 
-(defn emit-wrapped [& exprs]
+(defn wrap-fn [& exprs]
   (str "(function(){" (string/join exprs) "})()"))
 
 (defn comma-sep [args]
   (->> args (map emit) (string/join ",")))
 
+(defn wrap-quotes [s]
+  (str "\"" s "\""))
+
 ;; special forms
+
+(defmulti emit-special :op)
 
 (defn emit-aget [{:keys [target fields]}]
   (letfn [(emit-field [field] (str "[" (emit field) "]"))]
     (apply str (emit target) (map emit-field fields))))
 
-(defn emit-aset [{:keys [value] :as ast}]
+(defmethod emit-special :aget [ast]
+  (emit-aget ast))
+
+(defmethod emit-special :aset [{:keys [value] :as ast}]
   (str (emit-aget ast) "=" (emit value)))
 
-(defn emit-def [{:keys [name init]}]
+(defmethod emit-special :def [{:keys [name init]}]
   (str (emit name) "=" (emit init)))
 
-(defn emit-do [{:keys [env body]}]
+(defmethod emit-special :do [{:keys [env body]}]
   (if (= (:context env) :expr)
-      (emit-wrapped (emit-statements body))
+      (wrap-fn (emit-statements body))
       (emit-statements body)))
 
-(defn emit-if [{:keys [env test then else]}]
+(defmethod emit-special :if [{:keys [env test then else]}]
   (if (= (:context env) :expr)
       (str "((" (emit test) ")?" (emit then) ":" (emit else) ")")
       (str "if(" (emit test) "){" (emit then)
            "}else{" (emit else) "}")))
 
-(defn emit-new [{:keys [ctor args]}]
+(defmethod emit-special :new [{:keys [ctor args]}]
   (str "(new " (emit ctor) "(" (comma-sep args) "))"))
 
-(defn emit-throw [{:keys [thrown]}]
-  (emit-wrapped "throw " (emit thrown) ";\n"))
+(defmethod emit-special :throw [{:keys [thrown]}]
+  (wrap-fn "throw " (emit thrown) ";\n"))
 
 ;; function forms
 
@@ -67,7 +77,7 @@
   (str "case " num-params ":" (emit-params params)
        "return " (emit-statements body)))
 
-(defn emit-fn [{:keys [clauses]}]
+(defmethod emit-special :fn [{:keys [clauses]}]
   (if (= (count clauses) 1)
       (let [{:keys [params body]} (val (first clauses))]
         (str "(function(){" (emit-params params)
@@ -86,21 +96,21 @@
               (map (fn [[k v]] (str (emit-escaped k) "=" (emit v))))
               (string/join ";\n")) ";\n")))
 
-(defn emit-let [{:keys [env bindings body]}]
+(defmethod emit-special :let [{:keys [env bindings body]}]
   (if (= (:context env) :expr)
-      (emit-wrapped (emit-bindings bindings) (emit-statements body))
+      (wrap-fn (emit-bindings bindings) (emit-statements body))
       (str (emit-bindings bindings) (emit-statements body))))
 
-(defn emit-loop [{:keys [env bindings body]}]
+(defmethod emit-special :loop [{:keys [env bindings body]}]
   (if (= (:context env) :expr)
-      (emit-wrapped (emit-bindings bindings)
+      (wrap-fn (emit-bindings bindings)
                     "while(true){"
                     (emit-statements body)
                     "break;\n}")
       (str (emit-bindings bindings) "while(true){"
            (emit-statements body) "break;\n}")))
 
-(defn emit-recur [{:keys [args recur-point] :as ast}]
+(defmethod emit-special :recur [{:keys [args recur-point]}]
   (let [recur-bindings (:bindings recur-point)
         bindings
         (loop [bindings [] idx 0]
@@ -111,100 +121,83 @@
                 bindings)))]
     (str (emit-bindings bindings) "continue")))
 
-;; list forms
+;; collection forms
 
-(defn emit-fncall [callable args]
-  (str (emit callable) ".call(null," (string/join "," (map emit args)) ")"))
+(defmulti emit-collection :type)
 
-(defn emit-list [{:keys [children] {:keys [quoted?]} :env}]
+(defn emit-invoke [invoked args]
+  (str (emit invoked) ".call(null," (comma-sep args) ")"))
+
+(defmethod emit-collection :list [{:keys [children] {:keys [quoted?]} :env}]
   (if-let [{:keys [type value] :as first-child} (first children)]
     (if quoted?
         (str "cljs.core.list(" (comma-sep children) ")")
-        (emit-fncall first-child (rest children)))
+        (emit-invoke first-child (rest children)))
     "cljs.core.List.EMPTY"))
 
-;; other forms
-
-(defn emit-vector [{:keys [children]}]
+(defmethod emit-collection :vector [{:keys [children]}]
   (if (empty? children)
       "cljs.core.PersistentVector.EMPTY"
       (str "cljs.core.PersistentVector.fromArray(["
            (comma-sep children) "],true)")))
 
-(defn emit-map [{:keys [children]}]
+(defmethod emit-collection :map [{:keys [children]}]
   (if (empty? children)
       "cljs.core.PersistentArrayMap.EMPTY"
       (str "new cljs.core.PersistentArrayMap.fromArray(["
            (comma-sep children) "],true,false)")))
 
-(defn emit-set [{:keys [children]}]
+(defmethod emit-collection :set [{:keys [children]}]
   (if (empty? children)
       "cljs.core.PersistentHashSet.EMPTY"
       (str "cljs.core.PersistentHashSet.fromArray(["
            (comma-sep children) "],true)")))
 
-(defn emit-number [{:keys [form]}]
+;; constant forms
+
+(defmulti emit-constant :type)
+
+(defmethod emit-constant :bool [{:keys [form]}]
   (str form))
 
-(defn emit-keyword [{:keys [form]}]
+(defmethod emit-constant :keyword [{:keys [form]}]
   (let [name (name form)]
-    (str "new cljs.core.Keyword(null,\""
-         name "\",\"" name "\"," (hash form) ")")))
+    (str "new cljs.core.Keyword(null,"
+         (wrap-quotes name) "," (wrap-quotes name) "," (hash form))))
 
-(defn emit-string [{:keys [form]}]
-  (str "\"" form "\""))
+(defmethod emit-constant :nil [_]
+  "null")
 
-(defn emit-symbol [{:keys [form] {:keys [quoted?]} :env}]
-  (let [ns (namespace form)
-        name (name form)]
+(defmethod emit-constant :number [{:keys [form]}]
+  (str form))
+
+(defmethod emit-constant :string [{:keys [form]}]
+  (wrap-quotes form))
+
+(defmethod emit-constant :symbol [{:keys [form] {:keys [quoted?]} :env}]
+  (let [name (name form)
+        ns (namespace form)]
     (if quoted?
         (str "new cljs.core.Symbol("
-             (if ns (str "\"" ns "\"") "null") ",\"" name "\",\""
-             (str (when ns (str ns ".")) name) "\"," (hash form) ",null)")
+             (if ns (wrap-quotes ns) "null") ","
+             (wrap-quotes name) ","
+             (wrap-quotes (str (when ns (str ns ".")) name)) ","
+             (hash form) ",null)")
         (str (when (and ns (not= ns "js")) (str (emit-escaped ns) "."))
              (emit-escaped name)))))
 
-(defn emit-bool [{:keys [form]}]
-  (str form))
-
-(defn emit-nil [_]
-  "null")
-
 ;; generic interface
 
-(def emitters
-  {:list emit-list
-   :vector emit-vector
-   :map emit-map
-   :set emit-set
-   :number emit-number
-   :keyword emit-keyword
-   :string emit-string
-   :symbol emit-symbol
-   :bool emit-bool
-   :nil emit-nil
-   :aget emit-aget
-   :aset emit-aset
-   :def emit-def
-   :do emit-do
-   :fn emit-fn
-   :if emit-if
-   :let emit-let
-   :loop emit-loop
-   :new emit-new
-   :recur emit-recur
-   :throw emit-throw})
-
-(defn emit [{:keys [env op type] :as ast}]
-  (let [context (:context env)
-        return? (and (= context :return)
-                     (#{:aget :aset :const :coll :fn :new} op))]
-    (str (when return? "return ")
-         (if (#{:const :coll} op)
-             (let [emit-type (emitters type)]
-               (emit-type ast))
-             (let [emit-op (emitters op)]
-               (emit-op ast)))
+(defn emit
+  "Given an AST node `ast`, returns a string of equivalent JavaScript code."
+  [{:keys [env op] :as ast}]
+  (let [context (:context env)]
+    (str (when (and (= context :return)
+                    (#{:aget :aset :const :coll :fn :new} op)) "return ")
+         (condp = op
+           :const (emit-constant ast)
+           :coll (emit-collection ast)
+           (emit-special ast))
          (when-not (or (= context :expr)
                        (#{:if :let :loop} op)) ";\n"))))
  
