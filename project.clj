610c8adff0374b0b990c507955bfc026fd390a90
Max Kreminski <maxkreminski@gmail.com>
2014-10-24 15:44:49 -0700

    Bump dependency versions
    * ClojureScript 0.0-2202 => 0.0-2371
    * escodegen 1.3.2 => 1.4.1

--- project.clj
+++ project.clj
@@ -7,10 +7,10 @@
 
   :dependencies
   [[org.clojure/clojure "1.6.0"]
-   [org.clojure/clojurescript "0.0-2202"]]
+   [org.clojure/clojurescript "0.0-2371"]]
 
   :node-dependencies
-  [[escodegen "1.3.2"]]
+  [[escodegen "1.4.1"]]
 
   :plugins
   [[lein-cljsbuild "1.0.3"]
